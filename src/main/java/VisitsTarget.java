891ec976e14e3325817929bd8557e1b076216aad
Kevin-Batchelor <kevinscottbatchelor@gmail.com>
2022-07-31 22:32:52 -0700

    Added grouped anagrams and visits to meet target java interview questions.

--- src/main/java/VisitsTarget.java
+++ src/main/java/VisitsTarget.java
@@ -1,3 +1,14 @@
+/*  You are given an array of non-negative integers, visits, which represents
+*   how many people visited a website on consecutive days.
+*   Visits[0] is the number of visitors on the first day.
+*   Visits[1] is the number of visitors on the second day and so on.
+*
+*   Your task is to return the index i of the first day when the
+*   total number of visitors reaches a given target.
+*   So: visits[0] + visits[1] + visits[i] >= target. If the sum of all
+*   daily visits never reaches the target return -1.    */
+
+
 public class VisitsTarget {
     public static int solution(int[] visits, int target) {
 
